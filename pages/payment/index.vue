e50710c519836fbf4f718d3689d649fdef5f0bc3
Hubert <hubert@Hubert-MBP.local>
2020-07-31 19:26:33 +0800

    add payment list.

--- pages/payment/index.vue
+++ pages/payment/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="container">
-    Payment ID# is required.
+  <div>
+    There are {{ list.length }} voucher{{
+      list.length > 1 ? 's' : ''
+    }}.
+    <ul>
+      <li v-for="item in list" :key="item.id">
+        <nuxt-link :to="`/payment/${item.id}`">
+          # {{ item.id }} - ${{ item.price }}
+        </nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import payments from '~/static/payments.json'
 export default {
+  data() {
+    return {
+      list: payments.data
+    }
+  },
   head() {
     return {
       title: 'Payments'
